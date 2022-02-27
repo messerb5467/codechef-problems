50ac8f3079f0150f29b8fc7b4348a0ad59031bd2
Brad Messer <messerb5467@gmail.com>
2022-02-27 15:14:27 +0000

    Updated interactions between test_driver.py and test.py.

--- brush-fire/test.py
+++ brush-fire/test.py
@@ -1,40 +1,54 @@
 import networkx as nx
 import logging
 from bush import Bush
+from utils import get_ints_from_string
 
 class Test:
     def __init__(self,
-                 bush_graph,
-                 sentimental_bushes):
-        self.bush_graph = bush_graph
-        self.sentimental_bushes = sentimental_bushes
+                 file_context):
+        self.file_context = file_context
+        self.bush_graph = nx.DiGraph()
         self.fire_loop = 0
-
-    def get_burn_status(self):
-        return set([burn_status for _, burn_status in self.bush_graph.nodes(data='on_fire')])
+        self.__process_bush_relations()
         
-    def process_bush_relations(self,
-                               file,
-                               num_bushes,
-                               starting_burn):
-      for i in range(num_bushes):
-          node_and_edge_rels = file.readline().split(" ")
-          node_idx = node_and_edge_rels[0]
-          edge_rels = node_and_edge_rels[1::]
-          del node_and_edge_rels
+    def __process_bush_relations(self):
+        num_bushes, starting_burn, _ = get_ints_from_string(self.file_context.readline())
+        for i in range(num_bushes):
+            node_and_edge_rels = self.file_context.readline().split(" ")
+            node_idx = node_and_edge_rels[0]
+            edge_rels = node_and_edge_rels[1::]
+            del node_and_edge_rels
  
-          # Adding node_idx as part of the tuple here allows the data to be
-          # less strict under the covers.
-          if node_idx == starting_burn:
-              self.bush_graph.add_node((node_idx, Bush(node_idx, True, False)))
-          else:
-              self.bush_graph.add_node((node_idx, Bush(node_idx, False, False)))
-          # While the data likely uses an edge into the same node as a terminal
-          # index of sort, we won't need it for our use case.
-          bush_proximities = [(node_idx, i) for i in edge_rels if i != node_idx]
-          self.bush_graph.add_edges_from(bush_proximities)
+            # Adding node_idx as part of the tuple here allows the data to be
+            # less strict under the covers.
+            if node_idx == starting_burn:
+                self.bush_graph.add_node((node_idx, Bush(node_idx, True, False)))
+            else:
+                self.bush_graph.add_node((node_idx, Bush(node_idx, False, False)))
+            # While the data likely uses an edge into the same node as a terminal
+            # node, we won't need it for our use case since a bush can't reburn itself.
+            # A burnt bush is immediately removed from the graph after being considered
+            # burnt.
+            bush_proximities = [(node_idx, i) for i in edge_rels if i != node_idx]
+            self.bush_graph.add_edges_from(bush_proximities)
+        self.sentimental_bushes = get_ints_from_string(self.file_context.readline())
 
-    def spread_fire(self):
+    def __get_burn_status(self):
+        return set([burn_status for _, burn_status in self.bush_graph.nodes(data='on_fire')])
+
+    def __protect_bush(self):
+        sentimental_bush = self.sentimental_bushes[self.fire_loop]
+        bush_to_protect_node = self.bush_graph.nodes[sentimental_bush]
+        ancestor = nx.algorithms.dag.ancestors(self.bush_graph, 
+                                               bush_to_protect_node)
+        if ancestor:
+            # Current test data only allows for one ancestor node
+            # to a bunch of protected bushes. Could expand on in future
+            # by finding test data with multiple ancestors.
+            bush_to_protect_node = ancestor[0]
+        bush_to_protect_node['protect_status'] = True
+
+    def __spread_fire(self):
         bush_fire_status = self.bush_graph.nodes.data()
         on_fire_bushes = [node for node in bush_fire_status 
                                if node.get('on_fire') == True 
@@ -48,36 +62,17 @@ class Test:
         # edges too. Documentation is not clear for this function
         # but implies it will.
         self.bush_graph.remove_nodes_from(on_fire_bushes)
-
-    def protect_bush(self):
-        sentimental_bush = self.sentimental_bushes[self.fire_loop]
-        bush_to_protect_node = self.bush_graph.nodes[sentimental_bush]
-        ancestor = nx.algorithms.dag.ancestors(self.bush_graph, 
-                                               bush_to_protect_node)
-        if ancestor:
-            # Current test data only allows for one ancestor node
-            # to a bunch of protected bushes. Could expand on in future
-            # by finding test data with multiple ancestors.
-            bush_to_protect_node = ancestor[0]
-        bush_to_protect_node['protect_status'] = True
     
-    def successfully_protect_bushes(self):        
+    def __successfully_protect_bushes(self):        
         if self.bush_graph.number_of_nodes() >= len(self.sentimental_bushes) \
             and set(self.sentimental_bushes) in self.bush_graph.nodes:
             logging.info("Yes")
         elif self.bush_graph.number_of_nodes() < len(self.sentimental_bushes):
             logging.info("No")
     
-    def run_burn_simulation(self,
-                            file,
-                            num_bushes,
-                            starting_burn):
-        self.process_bush_relations(file,
-                                    num_bushes,
-                                    starting_burn)
-        burn_status = self.bush_graph.nodes
-        while(True in self.get_burn_status()):
-            self.protect_bush()
-            self.spread_fire()
+    def run_burn_simulation(self):
+        while(True in self.__get_burn_status()):
+            self.__protect_bush()
+            self.__spread_fire()
             self.fire_loop += 1            
-        self.successfully_protect_bushes()
+        self.__successfully_protect_bushes()
--- brush-fire/test_driver.py
+++ brush-fire/test_driver.py
@@ -1,8 +1,16 @@
-def save_bushes(test_file):
+import argparse
+from test import Test
+
+def process_parameters():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('test_file',
+                        help='A test file containing graph data to run burn simulations on.')
+    return parser.parse_args()
+
+def main():
+  test_file = process_parameters()
   with open(test_file) as f:
     num_tests = int(f.readline())
-    for i in range(0, num_tests):
-      bush_graph = nx.DiGraph()
-      num_bushes, starting_burn, num_bushes_to_save = get_ints_from_string(f.readline())
-      process_bush_relations(f, bush_graph, num_bushes, starting_burn)
-      sentimental_bushes = get_ints_from_string(f.readline())
+    for _ in range(num_tests):
+        tester = Test(f)
+        tester.run_burn_simulation()
